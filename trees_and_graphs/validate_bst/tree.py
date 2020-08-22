144f3fc1ef24e0d93fee6b568dc2f67c5758b8d3
nyasho4ka <nyasho4ka@gmail.com>
2020-08-22 14:28:28 +0500

    fix

--- trees_and_graphs/validate_bst/tree.py
+++ trees_and_graphs/validate_bst/tree.py
@@ -20,33 +20,6 @@ class Node:
                 return
             self.left.insert(value)
 
-    def max_depth(self, node):
-        if node is None:
-            return 0
-        left_depth = self.max_depth(node.left)
-        right_depth = self.max_depth(node.right)
-        if left_depth > right_depth:
-            return 1 + left_depth
-        return 1 + right_depth
-
-    def is_balanced(self):
-        tree_queue = collections.deque()
-        tree_queue.append(self)
-        while len(tree_queue) > 0:
-            elem = tree_queue.popleft()
-            max_size = self.visit(elem.left)
-            left_size = self.visit(elem.right)
-            if abs(max_size - left_size) > 1:
-                return False
-            if elem.left is not None:
-                tree_queue.append(elem.left)
-            if elem.right is not None:
-                tree_queue.append(elem.right)
-        return True
-
-    def visit(self, node):
-        return self.max_depth(node)
-
     # FIX REPRESENTING ALGO
     def __str__(self):
         return str(self.value)
@@ -67,4 +40,3 @@ tree.insert(10)
 tree.insert(11)
 tree.insert(12)
 tree.insert(4)
-print(tree.is_balanced())
